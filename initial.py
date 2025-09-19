6e6bc4fd7de59ed6b956c83e00768c79c85b8d42
stanley_565 Whitehouse <stanley.whitehouse25@imperial.ac.uk>
2025-09-19 18:25:17 +0100

    Stan Commit

--- initial.py
+++ initial.py
@@ -1,3 +1,5 @@
 #commit
 
 print("hello")
+
+#kfj
---
+++ research/data_cleaner_extractor.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import math
+import matplotlib as mp
+
+
+data = pd.read_csv('test.csv')
+df = pd.DataFrame(data)
+print(df.head())
+
